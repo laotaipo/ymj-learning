4e7aae0a8da4e309d1382c2fe0c4decee4e5734a
yangmj01 <1600812476@qq.com>
2022-03-09 10:27:19 +0800

    add: react

---
+++ react/my-app/src/App.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react'
+import Components from './components'
+
+function App() {
+    return (
+        <div className="app">
+            <Components></Components>
+        </div>
+    )
+}
+export default App
---
+++ react/my-app/src/components.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react'
+
+const name = 'yangmj'
+
+function Components() {
+    return (
+        <div className="components">
+            333333{name}
+            {2 + 2}
+        </div>
+    )
+}
+export default Components
