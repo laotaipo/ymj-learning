e6a946f63760e0cd89123bcb4b9871a77c37267c
wenxiaohua_edward <1600812476@qq.com>
2022-04-26 19:00:50 +0800

    add: searchsearch

---
+++ learn_algorithm/search/binarySearch.js
@@ -0,0 +1,19 @@
+Array.prototype.binarySearch = function (item) {
+    let low = 0
+    let high = this.length - 1
+    while (low <= high) {
+        const mid = Math.floor((low + high) / 2)
+        const ele = this[mid]
+        if (ele > item) {
+            high = mid - 1
+        } else if (ele < item) {
+            low = mid + 1
+        } else {
+            return mid
+        }
+    }
+    return -1
+}
+
+const arr = [1, 2, 3, 4, 5, 6, 7]
+console.log(arr.binarySearch(5))
