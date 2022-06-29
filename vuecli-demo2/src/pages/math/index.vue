1336449ae7daa29598496a55bcfb2e877114e82d
wenxiaohua_edward <1600812476@qq.com>
2022-06-29 11:26:03 +0800

    add: vue-cli demo

---
+++ vuecli-demo2/src/pages/math/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>math</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+@Component({})
+export default class Index extends Vue {
+	public created() {
+		console.log('math')
+	}
+}
+</script>
--- vuecli-demo2/vue.config.js
+++ vuecli-demo2/vue.config.js
@@ -1,4 +1,56 @@
+const path = require('path')
+const glob = require('glob')
+
+const context = process.env.CONTEXT || process.cwd()
+
+const regNodeModules = /node_modules/
+const cacheGroups = {
+	vendors: {
+		test: regNodeModules,
+		name: 'vendors',
+		minChunks: 5,
+		priority: -10,
+	},
+	common: {
+		test: module => !regNodeModules.test(module.context),
+		name: 'common',
+		minChunks: 5,
+		priority: -20,
+		reuseExistingChunk: true,
+	},
+}
+
+const entryFileName = 'entry.ts'
+const entryPath = 'src/pages'
+const entryPattern = `**/${entryFileName}`
+const cwd = path.resolve(context, entryPath)
+const pagesConfig = {}
+glob.sync(entryPattern, { cwd }).forEach(page => {
+	const key = page.replace(`/${entryFileName}`, '')
+	const entry = `${entryPath}/${page}`
+	const fileName = `${key}.html`
+	const chunks = Object.keys(cacheGroups).concat([key])
+	// const chunks = [key]
+	pagesConfig[key] = {
+		entry,
+		fileName,
+		chunks,
+	}
+})
+console.log(pagesConfig)
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
 	transpileDependencies: true,
+	pages: pagesConfig,
+	chainWebpack: config => {
+		config.optimization.splitChunks({
+			chunks: 'all',
+			name: false,
+			minSize: 20000,
+			cacheGroups: {
+				...cacheGroups,
+				default: false,
+			},
+		})
+	},
 })
