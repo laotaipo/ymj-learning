5c0b3985e4f5f0a091c7114e93227115b4b8d1e8
wenxiaohua_edward <1600812476@qq.com>
2022-06-29 14:15:16 +0800

    chore: electron项目

---
+++ remote-control/app/main/index.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const { app, BrowserWindow } = require('electron')
+const isDev = require('electron-is-dev')
+
+let win
+app.on('ready', () => {
+	win = new BrowserWindow({
+		width: 600,
+		height: 300,
+		webPreferences: {
+			nodeIntegration: true,
+		},
+	})
+	if (isDev) {
+		win.loadURL('http://localhost:3000')
+	} else {
+		window.loadFile(path.resolve(__dirname, '../renderer/pages/main/index.html'))
+	}
+})
